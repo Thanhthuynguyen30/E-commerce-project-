7c3360270b71fa292c2ba757badf9b599b34f006
Thanh Thuy Nguyen <149237392+Thanhthuynguyen30@users.noreply.github.com>
2023-12-15 17:32:35 +0700

    Update Data exploration.sql

--- Data exploration.sql
+++ Data exploration.sql
@@ -1,16 +1,3 @@
-## Number of null values in each collumn 
-
-SELECT COUNT(*) - COUNT(InvoiceNo) InvoiceNo,
- COUNT(*) - COUNT(StockCode) StockCode ,
- COUNT(*) - COUNT(Description) Description ,
- COUNT(*) - COUNT(Quantity) Quantity,
- COUNT(*) - COUNT(InvoiceDate) InvoiceDate ,
- COUNT(*) - COUNT(UnitPrice) UnitPrice ,
- COUNT(*) - COUNT(CustomerID) CustomerID ,
- COUNT(*) - COUNT(Country) Country,
-
-FROM `EcommerceProjectPractice.sales` ;
-
 ## Checking for the unique values
 
 SELECT COUNT(DISTINCT InvoiceNo) InvoiceNo,
@@ -22,3 +9,15 @@ SELECT COUNT(DISTINCT InvoiceNo) InvoiceNo,
        COUNT(DISTINCT CustomerID) CustomerID,
        COUNT(DISTINCT Country) Country
 FROM `EcommerceProjectPractice.sales`;
+
+## Checking null values in each column
+ 
+SELECT COUNT(*) - COUNT(InvoiceNo) InvoiceNo,
+       COUNT(*) - COUNT(StockCode) StockCode ,
+       COUNT(*) - COUNT(Description) Description ,
+       COUNT(*) - COUNT(Quantity) Quantity,
+       COUNT(*) - COUNT(InvoiceDate) InvoiceDate ,
+       COUNT(*) - COUNT(UnitPrice) UnitPrice ,
+       COUNT(*) - COUNT(CustomerID) CustomerID ,
+       COUNT(*) - COUNT(Country) Country,
+FROM `EcommerceProjectPractice.sales` ;
